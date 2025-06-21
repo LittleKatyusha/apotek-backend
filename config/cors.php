19592c7b7f9de06b9419207a6e7addcfd622efba
LittleKatyusha <abiyusuf2809@gmail.com>
2025-06-21 23:41:12 +0700

    cors http://localhost:8000

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://romanesco.it.com/'],
+    'allowed_origins' => ['https://romanesco.it.com', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => ['/^https:\/\/romanesco\.it\.com(\/.*)?$/'],
 
